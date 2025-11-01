9cadc30a3f24642486db733bb2080ca470572ebf
nathanaday <nraday1221@gmail.com>
2025-10-31 17:46:15 -0700

    shadcn added

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ The UI source code for `iot-data-sandbox`!
 
 ### Integrations
 
-Vue.js + Typesript + Vite
+Vue.js + Typescript + Vite
 
 Extras:
 - TailwindCSS (https://v2.tailwindcss.com/docs/guides/vue-3-vite)
-- shadcn (https://ui.shadcn.com/docs/installation/vite)
+- shadcn-vue (https://www.shadcn-vue.com/docs/installation/vite.html)
 
 
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 </script>
 
 <template>
-
+    <div class="flex justify-center items-center h-screen">
+        <Card>
+            <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <p>Testing that tailwindcss and shadcn are working</p>
+            </CardContent>
+        </Card>
+    </div>
 </template>
 
 <style scoped></style>
